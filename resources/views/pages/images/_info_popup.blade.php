553a3ee5a082418cb933008b249bae7a798c5f82
LostInProgres <mirthedh@gmail.com>
2024-05-05 23:19:24 +0200

    Initial Layouts stuff

--- resources/views/pages/images/_info_popup.blade.php
+++ resources/views/pages/images/_info_popup.blade.php
@@ -1,8 +1,8 @@
-<div class="gallery-popup card bg-secondary" style="width:90%; margin-left:5%;">
+<div class="gallery-popup card bg-light" style="width:90%; margin-left:5%;">
     <div class="card-header text-center">
         <img src="{{ $image->imageUrl }}" class="mw-100 mb-2" style="max-height: 85vh;" />
     </div>
-    <div class="card-body bg-light rounded-bottom">
+    <div class="card-body bg-secondary rounded-bottom">
         @include('pages.images._info_content')
     </div>
 </div>
